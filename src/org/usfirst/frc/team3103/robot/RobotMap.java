165110fd3c4e4d33e29906bbde809881c0c4c2ee
madisoncontreras <35384467+madisoncontreras@users.noreply.github.com>
2018-02-03 14:33:14 -0600

    I added a winch subsystem. You're welcome- Madison

--- src/org/usfirst/frc/team3103/robot/RobotMap.java
+++ src/org/usfirst/frc/team3103/robot/RobotMap.java
@@ -24,6 +24,10 @@ public class RobotMap {
 	public static int frMotor = 2;
 	public static int brMotor = 3;
 	public static int blMotor = 4;
+	
+	public static int wMotor1 = 12;
+	public static int wMotor2 = 13; 
+	
 
 	// If you are using multiple modules, make sure to define both the port
 	// number and the module. For example you with a rangefinder:
